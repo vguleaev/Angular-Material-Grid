28ce2e97f8edf384e1cee27e06c2873cd62155a8
Guleaev <gvladisl@techteam.com>
2018-10-22 12:11:50 +0300

    Fix dependencies for Angular Material v7

--- angular-material-grid/src/app/app.component.ts
+++ angular-material-grid/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { GridConfig } from './grid/grid-config';
-import { GridColumn } from './grid/grid-column';
-import { TableService } from './table.service';
 import { DataService } from './services/data.service';
 
 @Component({
@@ -14,53 +12,48 @@ export class AppComponent implements OnInit {
   gridOptions: GridConfig = new GridConfig();
 
   constructor(private dataService: DataService) {
+    this.gridOptions = {
+      searchPlaceholder: 'Search by name, username or email..',
+      source: this.dataService,
+      pageSize: 5,
+      mobileViewColumnIndex: 1,
+      rememberState: false,
+      filters: [],
+      columns: [{
+        name: 'id',
+        label: 'Id',
+        searchable: true,
+        sortable: true,
+        disabled: false,
+        content: (item: any) => item.id
+      },
+      {
+        name: 'name',
+        label: 'Name',
+        searchable: true,
+        sortable: true,
+        disabled: false,
+        content: (item: any) => item.name
+      },
+      {
+        name: 'email',
+        label: 'Email',
+        searchable: true,
+        sortable: true,
+        disabled: false,
+        content: (item: any) => item.email
+      },
+      {
+        name: 'company',
+        label: 'Company',
+        searchable: false,
+        sortable: false,
+        disabled: false,
+        content: (item: any) => item.company.name
+      }
+    ]} as GridConfig;
   }
 
   ngOnInit() {
-    this.gridOptions.searchPlaceholder = 'Search by name, username or email..';
-    this.gridOptions.source = this.dataService;
-    this.gridOptions.pageSize = 5;
-    this.gridOptions.mobileViewColumnIndex = 1;
-    this.gridOptions.columns = new Array<GridColumn>();
-    this.gridOptions.columns.push({
-      name: 'id',
-      label: 'Id',
-      searchable: true,
-      sortable: true,
-      disabled: false,
-      content: (item: any) => item.id
-    });
-    this.gridOptions.columns.push({
-      name: 'name',
-      label: 'Name',
-      searchable: true,
-      sortable: true,
-      disabled: false,
-      content: (item: any) => item.name
-    });
-    this.gridOptions.columns.push({
-      name: 'username',
-      label: 'Username',
-      searchable: true,
-      sortable: true,
-      disabled: false,
-      content: (item: any) => item.username
-    });
-    this.gridOptions.columns.push({
-      name: 'email',
-      label: 'Email',
-      searchable: true,
-      sortable: true,
-      disabled: false,
-      content: (item: any) => item.email
-    });
-    this.gridOptions.columns.push({
-      name: 'company',
-      label: 'Company',
-      searchable: false,
-      sortable: false,
-      disabled: false,
-      content: (item: any) => item.company.name
-    });
   }
 }
